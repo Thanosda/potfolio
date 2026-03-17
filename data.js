e5d6bd2f5aaf7efb5350c8030ac8a149af8af79c
Dhivagar <rdhivagar2200@gmail.com>
2026-03-17 23:10:18 +0530

    Update data.js

--- data.js
+++ data.js
@@ -39,7 +39,7 @@ const portfolioData = {
       description: "An AI-powered office attendance system using graph algorithms to ensure accurate tracking, streamlined management, and efficient monitoring of employee presence and productivity",
       techStack: ["HTML", "CSS", "PostgreSQL", "JavaScript"],
       githubUrl: "https://github.com/Thanosda/Office-Attendence.git",
-      liveUrl: "https://attendence-pro.netlify.app",
+      liveUrl: "https://grocer-snap.netlify.app",
       image: "/img/office.png",
      status: "completed"
     },
